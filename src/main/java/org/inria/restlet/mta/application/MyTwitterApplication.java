4e140a98bd5196abe4154c22363689dc8fb99437
Jean Lagniaux <34351725+jeanlagniaux@users.noreply.github.com>
2020-12-01 11:31:05 +0100

    initialisation commit

---
+++ src/main/java/org/inria/restlet/mta/application/MyTwitterApplication.java
@@ -0,0 +1,38 @@
+package org.inria.restlet.mta.application;
+
+
+import org.inria.restlet.mta.resources.TweetRessource;
+import org.inria.restlet.mta.resources.UserResource;
+import org.inria.restlet.mta.resources.UsersResource;
+import org.restlet.Application;
+import org.restlet.Context;
+import org.restlet.Restlet;
+import org.restlet.routing.Router;
+
+/**
+ *
+ * Application.
+ *
+ * @author msimonin
+ *
+ *
+ */
+public class MyTwitterApplication extends Application
+{
+//test
+    public MyTwitterApplication(Context context)
+    {
+        super(context);
+    }
+
+    @Override
+    public Restlet createInboundRoot()
+    {
+        Router router = new Router(getContext());
+        router.attach("/users", UsersResource.class);
+        router.attach("/users/{userId}", UserResource.class);
+        router.attach("/users/{userId}/tweets", TweetRessource.class);
+        router.attach("/users/tweets", UsersResource.class);
+        return router;
+    }
+}
---
+++ src/main/java/org/inria/restlet/mta/resources/UserResource.java
@@ -0,0 +1,77 @@
+package org.inria.restlet.mta.resources;
+
+import org.inria.restlet.mta.backend.Backend;
+import org.inria.restlet.mta.internals.User;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.ServerResource;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.restlet.ext.json.JsonRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.resource.Delete;
+import org.restlet.resource.Get;
+import org.restlet.resource.Post;
+import org.restlet.resource.ServerResource;
+
+/**
+ *
+ * Resource exposing a user.
+ *
+ * @author msimonin
+ * @author ctedeschi
+ *
+ */
+public class UserResource extends ServerResource
+{
+
+    /** Backend.*/
+    private Backend backend_;
+
+    /** Utilisateur géré par cette resource.*/
+    private User user_;
+
+    /**
+     * Constructor.
+     * Call for every single user request.
+     */
+    public UserResource()
+    {
+        backend_ = (Backend) getApplication().getContext().getAttributes()
+                .get("backend");
+    }
+
+
+    @Get("json")
+    public Representation getUser() throws Exception
+    {
+        String userIdString = (String) getRequest().getAttributes().get("userId");
+        int userId = Integer.valueOf(userIdString);
+        user_ = backend_.getDatabase().getUser(userId);
+
+        JSONObject userObject = new JSONObject();
+        userObject.put("name", user_.getName());
+        userObject.put("age", user_.getAge());
+        userObject.put("id", user_.getId());
+
+        return new JsonRepresentation(userObject);
+    }
+    
+    @Delete("json")
+	public Representation deleteUser(JsonRepresentation representation) throws Exception {
+		String userIdString = (String) getRequest().getAttributes().get("userId");
+		int userId = Integer.valueOf(userIdString);
+		if (backend_.getDatabase().exists(userId)) {
+			backend_.getDatabase().deleteUser(userId);
+			JSONObject response = new JSONObject();
+			response.put("result", "DONE");
+			return new JsonRepresentation(response);
+		}
+		else {
+			JSONObject response = new JSONObject();
+			response.put("result", "DOES NOT EXIST");
+			return new JsonRepresentation(response);
+		}
+	}
+
+}
