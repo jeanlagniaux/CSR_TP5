46839549a5ecccd3bad2a963ca760777630a5f63
theod <theod@DESKTOP-P6SCMB9>
2020-12-01 12:02:07 +0100

    commit constructeur zone

--- src/main/java/org/inria/restlet/mta/backend/Zone.java
+++ src/main/java/org/inria/restlet/mta/backend/Zone.java
@@ -1,8 +1,14 @@
 package org.inria.restlet.mta.backend;
 
+import java.util.Random;
+
 public class Zone {
 
+	private int nbSardine;
+	
 	public Zone() {
+		Random rand = new Random();
+		this.nbSardine = rand.nextInt(6);
 	}
 
 }
