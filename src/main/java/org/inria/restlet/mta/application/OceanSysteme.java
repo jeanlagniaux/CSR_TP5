0963f39adb6a77f83ab6823c18cbb45ad67e6c6c
Jean Lagniaux <34351725+jeanlagniaux@users.noreply.github.com>
2020-12-02 19:12:26 +0100

    comment

--- src/main/java/org/inria/restlet/mta/application/OceanSysteme.java
+++ src/main/java/org/inria/restlet/mta/application/OceanSysteme.java
@@ -10,32 +10,32 @@ class OceanSysteme {
 	static final int MAX_CLIENTS = 10;
 
 	/* Attributs */
-
-	private Ocean ocean = new Ocean();
-	private Client[] clients = new Client[MAX_CLIENTS];
-	private Camion camion = null;
-	private int nbClients = 0;
+//
+//	private Ocean ocean = new Ocean();
+//	private Client[] clients = new Client[MAX_CLIENTS];
+//	private Camion camion = null;
+//	private int nbClients = 0;
 
 	/* Constructeur du systeme d'emprunt */
 	OceanSysteme() {
 
-		/* Instanciation des sites */
-		for (int i = 0; i < NB_SITES; i++) {
-			sites[i] = new Site(i);
-		}
-		System.out.println("les " + sites.length + " sites ont été crées");
-
-		Random r = new Random();
-		/* Instanciation des clients */
-		for (int i = 0; i < MAX_CLIENTS; i++) {
-			int siteDepId = r.nextInt(NB_SITES); // ajout de + 1 pour avoir le dernier site
-			int siteArrId = r.nextInt(NB_SITES);
-			clients[i] = new Client(sites[siteDepId], sites[siteArrId], i);
-			clients[i].start();
-		}
-		/* Instanciation du camion */
-		camion = new Camion(sites, 10);
-		camion.start();
+//		/* Instanciation des sites */
+//		for (int i = 0; i < NB_SITES; i++) {
+//			sites[i] = new Site(i);
+//		}
+//		System.out.println("les " + sites.length + " sites ont été crées");
+//
+//		Random r = new Random();
+//		/* Instanciation des clients */
+//		for (int i = 0; i < MAX_CLIENTS; i++) {
+//			int siteDepId = r.nextInt(NB_SITES); // ajout de + 1 pour avoir le dernier site
+//			int siteArrId = r.nextInt(NB_SITES);
+//			clients[i] = new Client(sites[siteDepId], sites[siteArrId], i);
+//			clients[i].start();
+//		}
+//		/* Instanciation du camion */
+//		camion = new Camion(sites, 10);
+//		camion.start();
 
 	}
 
