b0f0596f1787f40464405c10149bc3c07ec2c35b
Jean Lagniaux <34351725+jeanlagniaux@users.noreply.github.com>
2020-12-02 17:29:20 +0100

    creation du main de test de la version 1

---
+++ src/main/java/org/inria/restlet/mta/application/OceanSysteme.java
@@ -0,0 +1,34 @@
+package org.inria.restlet.mta.application;
+
+import java.util.Random;
+
+class OceanSysteme {
+
+	/* Constantes */
+
+	static final int NB_SITES = 5;
+	static final int MAX_CLIENTS = 10;
+
+	/* Attributs */
+
+//	private Site[] sites = new Site[NB_SITES];
+//	private Client[] clients = new Client[MAX_CLIENTS];
+//	private Camion camion = null;
+//	private int nbClients = 0;
+
+	/* Constructeur du systeme d'emprunt */
+	OceanSysteme() {
+		
+		//TODO FAIRE RUN LE SYSTEME
+
+	}
+
+	/* Point d'entree du programme */
+
+	public static void main(String[] args) {
+
+		new OceanSysteme();
+
+	}
+
+} // class SystemeEmprunt
